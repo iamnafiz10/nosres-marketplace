4687044a398ceb17b383aa2429d8c63b45dfc2d7
Roshan SaiHam Nafiz <118830871+iamroshansaihamnafiz@users.noreply.github.com>
2024-05-07 20:46:22 +0600

    Upload

--- src/app/(USER Access)/profile-two/page.tsx
+++ src/app/(USER Access)/profile-two/page.tsx
@@ -405,8 +405,8 @@ function Page() {
                                                                             xmlns="http://www.w3.org/2000/svg"
                                                                             viewBox="0 0 24 24"
                                                                             fill="none" stroke="#6B7280"
-                                                                            strokewidth="1.5" strokelinecap="round"
-                                                                            strokelinejoin="round">
+                                                                            strokeWidth="1.5" strokeLinecap="round"
+                                                                            strokeLinejoin="round">
                                                                             <circle cx="12" cy="12" r="10"/>
                                                                             <path d="m4.9 4.9 14.2 14.2"/>
                                                                         </svg>
