8a5ca8e5990528b2ce5ffbf3d07106997cdb2fbc
Roshan SaiHam Nafiz <118830871+iamroshansaihamnafiz@users.noreply.github.com>
2024-05-11 11:13:26 +0600

    Done

--- src/app/(USER Access)/search-all/page.tsx
+++ src/app/(USER Access)/search-all/page.tsx
@@ -1551,7 +1551,7 @@ export default function Home() {
                                                 )}
                                             </div>
                                         </div>
-                                        <div className="box py-4 px-6 bg-white rounded mt-2">
+                                        <div className="box py-4 px-6 bg-white rounded mt-1">
                                             <div
                                                 className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4 pb-4">
                                                 <div className="col border rounded p-4 cursor-pointer">
