5c86ff19dadb48e001030c0c87726997ffd67c64
Roshan SaiHam Nafiz <118830871+iamroshansaihamnafiz@users.noreply.github.com>
2024-05-26 08:34:11 +0600

    d

--- src/app/(USER Access)/rond/page.tsx
+++ src/app/(USER Access)/rond/page.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 function Page() {
     return (
         <>
-            <h1 className="text-[20px] pt-[80px]">
-                Fiverr doesn't allow payment outside. But If you still want send me a message on my WhatsApp.
-                (+880) 01321764096
-
-                (Do first)
-            </h1>
+         <div>
+             <h1 className="text-[20px] pt-[80px]">
+                 Fiverr doesn't allow payment outside. But If you still want send me a message on my WhatsApp.
+                 (+880) 01321764096
+                 (Do first)
+             </h1>
+         </div>
         </>
     );
 }
