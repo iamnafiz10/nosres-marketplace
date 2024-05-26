e6653b88b40e02f45b9a2f174e0009e05a12b701
Roshan SaiHam Nafiz <118830871+iamroshansaihamnafiz@users.noreply.github.com>
2024-05-26 08:36:26 +0600

    P

--- src/app/(USER Access)/rond/page.tsx
+++ src/app/(USER Access)/rond/page.tsx
@@ -5,9 +5,11 @@ function Page() {
         <>
          <div>
              <h1 className="text-[20px] pt-[80px]">
-                 Fiverr doesn't allow payment outside. But If you still want send me a message on my WhatsApp.
+                 Fiverr doesn not allow payment outside. But If you still want send me a message on my WhatsApp.
                  (+880) 01321764096
-                 (Do first)
+                 (Do first).
+
+                 Also, Please don't send any message about outside payment.
              </h1>
          </div>
         </>
