4ac5db3d29ddac9f85dc317b5c0bc8bba4e4b6bc
Roshan SaiHam Nafiz <118830871+iamroshansaihamnafiz@users.noreply.github.com>
2024-05-26 08:39:10 +0600

    Up

--- src/app/(USER Access)/rond/page.tsx
+++ src/app/(USER Access)/rond/page.tsx
@@ -3,15 +3,21 @@ import React from 'react';
 function Page() {
     return (
         <>
-         <div>
-             <h1 className="text-[20px] pt-[80px]">
-                 Fiverr doesn not allow payment outside. But If you still want send me a message on my WhatsApp.
-                 (+880) 01321764096
-                 (Do first).
+            <div>
+                <h1 className="text-[20px] pt-[80px]">
+                    Fiverr dose not allow payment outside. But If you still want send me a message on my WhatsApp.
+                    (+880) 01321764096
+                    (Do first).
+                    <br/>
+                    <br/>
 
-                 Also, Please don't send any message about outside payment.
-             </h1>
-         </div>
+                    Also, Please don not send any message about outside payment.
+                    Just send me a message call: Got it.
+                    <br/>
+                    <br/>
+                    Then I will go for check my WhatsApp
+                </h1>
+            </div>
         </>
     );
 }
